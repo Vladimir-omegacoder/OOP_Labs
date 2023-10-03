53e99f1b06963a6116627dab03f8331dbb210ab9
Vladimir <supervova0505@gmail.com>
2023-10-03 23:22:44 +0300

    Final commit

--- OOP_Lab1.cpp
+++ OOP_Lab1.cpp
@@ -8,17 +8,22 @@
 int main()
 {
 
-    Rectangle rect(Point{ 10, 30 }, 200, 100);
+    Rectangle rectangle     (Point{ 10, 30 }, 200, 100);
+    Triangle  triangle      (Point{ 4, 2 }, Point{ 6, 8 }, Point{ 10, 3 });
+    Circle    circle        (Point{ 31, 82 }, 5.4f);
 
-    rect.Draw();
+    size_t shapes_size = 3;
 
-    Triangle tr(Point{ 4, 2 }, Point{ 6, 8 }, Point{ 10, 3 });
+    Shape** shapes = new Shape*[shapes_size]{ &rectangle, &triangle, &circle };
 
-    tr.Draw();
 
-    Circle circle(Point{ 31, 82 }, 5.4);
 
-    circle.Draw();
+    for (size_t i = 0; i < shapes_size; ++i)
+    {
+        shapes[i]->Draw();
+    }
+
+    return 0;
 
 }
 
