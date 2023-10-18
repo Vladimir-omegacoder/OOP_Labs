3a46809761e2bbfa9328803601a395e651792469
Vladimir <supervova0505@gmail.com>
2023-10-18 21:43:02 +0300

    Shape class created

---
+++ Shape.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Vector.h"
+
+
+
+
+
+class Shape
+{
+
+	virtual void draw() = 0;
+
+	virtual void move(const Vector2f& offset) = 0;
+
+	virtual void rotate(float angle) = 0;
+
+	virtual void scale(const Vector2f& factor) = 0;
+
+};
+
