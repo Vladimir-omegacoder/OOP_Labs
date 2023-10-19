a43a7b8243af15306928e45ae5520b7f7133f331
Vladimir <supervova0505@gmail.com>
2023-10-19 12:44:16 +0300

    Ctor with parameters added

--- Transform.h
+++ Transform.h
@@ -27,6 +27,9 @@ public:
 	Transform(): 
 		m_matrix{ 1, 0, 0,  0, 1, 0,  0, 0, 1 }{}
 
+	Transform(float a00, float a01, float a02, float a10, float a11, float a12, float a20, float a21, float a22) :
+		m_matrix{ a00, a01, a02,  a10, a11, a12,  a20, a21, a22 } {}
+
 
 
 	Transform& move(const Vector2f& offset);
