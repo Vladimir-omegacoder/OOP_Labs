44d3d85294c7d2b5b269356c5619a149dfe55f51
Vladimir <supervova0505@gmail.com>
2023-11-15 10:36:14 +0200

    Constructors, operator=, destructor

--- Smart_ptr.h
+++ Smart_ptr.h
@@ -10,6 +10,38 @@ template <class T>
 class Smart_ptr
 {
 
+private:
+
+	T* m_ptr;
+
+
+public:
+
+	Smart_ptr() : m_ptr(nullptr) {}
+
+	Smart_ptr(T* ptr) : m_ptr(ptr) {}
+
+	Smart_ptr(const Smart_ptr&) = delete;
+	Smart_ptr& operator=(const Smart_ptr&) = delete;
+	
+	Smart_ptr(Smart_ptr&& other) : m_ptr(other.m_ptr)
+	{
+		other.m_ptr = nullptr;
+	}
+	Smart_ptr& operator=(Smart_ptr&& other)
+	{
+		m_ptr = other.m_ptr;
+		other.m_ptr = nullptr;
+		return *this;
+	}
+
+	~Smart_ptr()
+	{
+		delete m_ptr;
+	}
+
+
+
 
 
 };
