382c02b2d97f61740d2ae36e25869740a3643056
Vladimir <supervova0505@gmail.com>
2023-12-06 14:56:11 +0200

    Initial cleaning

--- OOP_Lab4.cpp
+++ OOP_Lab4.cpp
@@ -1,10 +1,34 @@
 ﻿#include <iostream>
+#include "SFML/Graphics.hpp"
 
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	sf::RenderWindow main_window(sf::VideoMode(800, 600), "Graphics window");
+
+	while (main_window.isOpen())
+	{
+
+		sf::Event main_event;
+
+		while (main_window.pollEvent(main_event))
+		{
+
+			if (main_event.type == sf::Event::Closed)
+			{
+
+				main_window.close();
+				
+			}
+
+		}
+		
+		main_window.display();
+
+	}
+
+	return 0;
+
 }
-
-
