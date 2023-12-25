e744c003f5c35b7be354a40da42b9fabdf58bf33
Vladimir <supervova0505@gmail.com>
2023-12-25 16:17:18 +0200

    Default resources folder

--- Interface/Controls.h
+++ Interface/Controls.h
@@ -2,30 +2,70 @@
 
 
 
-#ifndef CONTROLS_FONT
-#define CONTROLS_FONT "../../../resources/default/fonts/calibri.ttf"
-#endif
 
 
 
 
+#ifndef _GUI_CONTROL
 
-#ifndef _CONTROL
-#define _CONTROL
+#define _GUI_CONTROL
 #include "Interface/include/Control.h"
+
+#ifndef DEFAULT_CONTROLS_FONT
+#define DEFAULT_CONTROLS_FONT "../../../resources/default/fonts/calibri.ttf"
 #endif
 
-#ifndef _BUTTON
-#define _BUTTON
+#endif
+
+
+
+
+
+#ifndef _GUI_BUTTON
+
+#define _GUI_BUTTON
 #include "Interface/include/Button.h"
+
+#ifndef DEFAULT_BUTTON_TEXTURE
+#define DEFAULT_BUTTON_TEXTURE "../resources/default/button/button_default.png"
 #endif
 
-#ifndef _CHECKBOX
-#define _CHECKBOX
+#ifndef DEFAULT_BUTTON_PRESSED_TEXTURE
+#define DEFAULT_BUTTON_PRESSED_TEXTURE "../resources/default/button/button_pressed_default.png"
+#endif
+
+#endif
+
+
+
+
+
+#ifndef _GUI_CHECKBOX
+
+#define _GUI_CHECKBOX
 #include "Interface/include/Checkbox.h"
+
+#ifndef DEFAULT_CHECKBOX_TEXTURE
+#define DEFAULT_CHECKBOX_TEXTURE "../resources/default/checkbox/checkbox_default.png"
 #endif
 
-#ifndef _TEXTBOX
-#define _TEXTBOX
+#ifndef DEFAULT_CHECKBOX_CHECKED_TEXTURE
+#define DEFAULT_CHECKBOX_CHECKED_TEXTURE "../resources/default/checkbox/checkbox_checked_default.png"
+#endif
+
+#endif
+
+
+
+
+
+#ifndef _GUI_TEXTBOX
+
+#define _GUI_TEXTBOX
 #include "Interface/include/Textbox.h"
+
+#ifndef DEFAULT_TEXTBOX_TEXTURE
+#define DEFAULT_TEXTBOX_TEXTURE "../resources/default/textbox/textbox_default.png"
+#endif
+
 #endif
