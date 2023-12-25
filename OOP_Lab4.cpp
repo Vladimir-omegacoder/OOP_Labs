79680dd33b958cdf2f552eb561bc76ae31a3221a
Vladimir <supervova0505@gmail.com>
2023-12-25 14:49:56 +0200

    Cleaning

--- OOP_Lab4.cpp
+++ OOP_Lab4.cpp
@@ -4,98 +4,14 @@
 
 
 
-#define TEXTBOX_TEXTURE "text_box1.png"
 
 
-
-void enter_symbol(Textbox& textbox, uint32_t character)
-{
-
-	if (character > 25 && character < 36)
-	{
-		if (textbox.get_text().getString().getSize() < 6)
-		{
-			sf::String temp = textbox.get_text().getString();
-			temp += character + 22;
-			textbox.get_text().setString(temp);
-
-		}
-	}
-
-	if (character == 59)
-	{
-		if (textbox.get_text().getString().getSize() > 0)
-		{
-			sf::String temp = textbox.get_text().getString();
-			temp.erase(temp.getSize() - 1, 1);
-			textbox.get_text().setString(temp);
-		}
-	}
-
-}
-
-class CursorChangeTextboxEventArgs : public TextboxEventArgs
-{
-
-public:
-
-	sf::RenderWindow& window;
-	const sf::Cursor& new_cursor;
-
-	CursorChangeTextboxEventArgs(Event_type event_type, sf::RenderWindow& window, const sf::Cursor& new_cursor)
-		: TextboxEventArgs(event_type), window(window), new_cursor(new_cursor) {}
-
-};
 
 
 int main()
 {
 
 	sf::RenderWindow main_window(sf::VideoMode(800, 600), "Graphics window");
-
-	sf::Texture textbox1_texture;
-	textbox1_texture.loadFromFile(TEXTBOX_TEXTURE);
-	sf::Font font;
-	font.loadFromFile(CONTROLS_FONT);
-	sf::RectangleShape textbox_graphics(sf::Vector2f(300, 50));
-	textbox_graphics.setPosition(300, 300);
-	textbox_graphics.setTexture(&textbox1_texture);
-
-	Textbox textbox;
-	textbox.get_graphics() = textbox_graphics;
-	textbox.get_text().setFont(font);
-	textbox.get_text().setFillColor(sf::Color::Blue);
-	textbox.get_text().setCharacterSize(50);
-	textbox.get_text().setPosition(300, 295);
-
-	sf::Cursor text_cursor;
-	text_cursor.loadFromSystem(sf::Cursor::Text);
-
-	void(*hover_cursor)(Textbox*, TextboxEventArgs*) = [](Textbox* textbox, TextboxEventArgs* args)
-		{
-
-			if (auto change_cursor = dynamic_cast<CursorChangeTextboxEventArgs*>(args))
-			{
-				sf::Cursor text_cursor;
-				text_cursor.loadFromSystem(sf::Cursor::Text);
-				change_cursor->window.setMouseCursor(text_cursor);
-			}
-
-		};
-	textbox.add_event_handler(hover_cursor, TextboxEventArgs::CURSOR_HOVER);
-
-	void(*unhover_cursor)(Textbox*, TextboxEventArgs*) = [](Textbox* textbox, TextboxEventArgs* args)
-		{
-
-			if (auto change_cursor = dynamic_cast<CursorChangeTextboxEventArgs*>(args))
-			{
-				sf::Cursor arrow_cursor;
-				arrow_cursor.loadFromSystem(sf::Cursor::Arrow);
-				change_cursor->window.setMouseCursor(arrow_cursor);
-			}
-
-		};
-	textbox.add_event_handler(unhover_cursor, TextboxEventArgs::CURSOR_AWAY);
 
 
 
@@ -114,60 +30,10 @@ int main()
 				
 			}
 
-			if (main_event.type == sf::Event::MouseMoved)
-			{
-
-				if (!textbox.is_hovered())
-				{
-					CursorChangeTextboxEventArgs cur_chng_args(TextboxEventArgs::CURSOR_HOVER, main_window, text_cursor);
-					textbox.try_hover(sf::Mouse::getPosition(main_window), &cur_chng_args);
-				}
-				else
-				{
-					CursorChangeTextboxEventArgs cur_chng_args(TextboxEventArgs::CURSOR_AWAY, main_window, text_cursor);
-					textbox.try_unhover(sf::Mouse::getPosition(main_window), &cur_chng_args);
-				}
-
-			}
-
-			if (main_event.type == sf::Event::MouseButtonPressed)
-			{
-
-				if (main_event.mouseButton.button == sf::Mouse::Left)
-				{
-
-					textbox.try_click();
-
-				}
-
-			}
-
-			if (main_event.type == sf::Event::MouseButtonReleased)
-			{
-
-				if (main_event.mouseButton.button == sf::Mouse::Left)
-				{
-
-					textbox.try_release();
-
-				}
-
-			}
-
-			if (main_event.type == sf::Event::KeyPressed)
-			{
-
-				if (textbox.is_active())
-				{
-					enter_symbol(textbox, main_event.key.code);
-				}
-
-			}
-
 		}
 
 
-		main_window.draw(textbox);
+		
 
 		
 		main_window.display();
